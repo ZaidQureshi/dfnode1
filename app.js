2d1aad4d08856cc7fd4632a7956877f6e000e758
Zaid Qureshi <zaidhorse@gmail.com>
2013-03-01 23:13:11 -0500

    awesome

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var http = require("http");
 var counter = 0;
+var port = process.env.PORT || 8888;
 http.createServer(function(req, res){
 	//var result = longlongProcess();
-	counter++;
+	//counter++;
 	res.writeHead(200, {"Content-Type": "text/html"});
-	res.write(counter);
+	res.write((counter++).toString());
+	console.log(counter);
 	res.end();
-}).listen(process.env.PORT || 8888);
+}).listen(port);
