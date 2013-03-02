28358b5478b0af5b33ca4a9bdf9b586f412b0306
Zaid Qureshi <zaidhorse@gmail.com>
2013-03-01 23:37:43 -0500

    'hi'

--- app.js
+++ app.js
@@ -2,11 +2,23 @@ var http = require("http");
 var counter = 0;
 var port = process.env.PORT || 8888;
 http.createServer(function(req, res){
-	//var result = longlongProcess();
-	counter++;
+	
+	counter=counter+1;
 	res.writeHead(200, {"Content-Type": "text/html"});
-	counter=counter-2;
-	res.write(counter.toString());
-	console.log(counter);
+	if (counter > 1)
+	{
+		res.write((counter-1).toString());
+		console.log(counter-1);
+	}
+	else if (counter > 2)
+	{
+		res.write((counter-2).toString());
+		console.log(counter-2);
+	}
+	else
+	{
+		res.write(counter.toString());
+		console.log(counter);	
+	}
 	res.end();
 }).listen(port);
